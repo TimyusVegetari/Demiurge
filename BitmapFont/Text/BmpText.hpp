cf4a77d7ba52a9881d5cb975b5caf2516137ef35
TimyusVegetari <ant110283@hotmail.fr>
2014-12-18 17:07:15 +0100

    Creation of classes to display a text with a bitmap font
    Creation of a class to load and store a bitmap font from .png files.
    And creation of a class to display a text with this type of font.
    
    Signed-off-by: TimyusVegetari <ant110283@hotmail.fr>

---
+++ BitmapFont/Text/BmpText.hpp
@@ -0,0 +1,291 @@
+////////////////////////////////////////////////////////////
+//
+// This file is part of Demiurge.
+// Copyright (C) 2013-2014 Acroute Anthony (ant110283@hotmail.fr)
+//
+// Demiurge is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// Demiurge is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Demiurge.  If not, see <http://www.gnu.org/licenses/>.
+//
+////////////////////////////////////////////////////////////
+
+#ifndef DRIMI_BITMAPFONT_TEXT_HPP__
+#define DRIMI_BITMAPFONT_TEXT_HPP__
+
+////////////////////////////////////////////////////////////
+// Headers
+////////////////////////////////////////////////////////////
+#include <SFML/Graphics/Export.hpp>
+#include <SFML/Graphics/Drawable.hpp>
+#include <SFML/Graphics/Transformable.hpp>
+#include "../Font/BmpFont.hpp"
+#include <SFML/Graphics/Rect.hpp>
+#include <SFML/Graphics/VertexArray.hpp>
+#include <string>
+
+namespace drimi {
+////////////////////////////////////////////////////////////
+/// \brief Graphical bitmap text can be drawn to a render target
+///
+////////////////////////////////////////////////////////////
+class BmpText : public sf::Drawable, public sf::Transformable {
+  private:
+
+    ////////////////////////////////////////////////////////////
+    // Member data
+    ////////////////////////////////////////////////////////////
+    sf::String          m_sfString;         ///< String to display
+    const BmpFont*      m_pBmpFont;         ///< Font used to display the string
+    GLuint              m_uiGlyphAdvance;   ///< Horizontal space between characters
+    GLfloat             m_fCharacterScale;  ///< Base scale of characters
+    sf::Uint32          m_uiStyle;          ///< Text style (see sf::Text::Style enum)
+    sf::Color           m_sfColor;          ///< Text color
+    sf::VertexArray     m_sfVertices;       ///< Vertex array containing the text's geometry
+    sf::FloatRect       m_sfBounds;         ///< Bounding rectangle of the text (in local coordinates)
+
+  public:
+
+    ////////////////////////////////////////////////////////////
+    // Constructor(s)/Destructor
+    ////////////////////////////////////////////////////////////
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Default contructor
+    ///
+    /// Create a text empty.
+    ///
+    ////////////////////////////////////////////////////////////
+    BmpText ( void );
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Construct the text from a string, font and scale
+    ///
+    /// \param szString       Text assigned to the string
+    /// \param oBmpFont       Font used to draw the string
+    /// \param fScale         Case scale of characters
+    ///
+    ////////////////////////////////////////////////////////////
+    BmpText ( const sf::String& sfString, const BmpFont& oBmpFont, GLuint uiCharacterScale = 1 );
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Destructor
+    ///
+    /// Destruct the text.
+    ///
+    ////////////////////////////////////////////////////////////
+    ~BmpText ( void );
+
+    ////////////////////////////////////////////////////////////
+    // General methods
+    ////////////////////////////////////////////////////////////
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Return the position of the \a index-th character
+    ///
+    /// This function computes the visual position of a character
+    /// from its index in the string. The returned position is
+    /// in global coordinates (translation, rotation, scale and
+    /// origin are applied).
+    /// If \a index is out of range, the position of the end of
+    /// the string is returned.
+    ///
+    /// \param index Index of the character
+    ///
+    /// \return Position of the character
+    ///
+    ////////////////////////////////////////////////////////////
+    sf::Vector2f FindCharacterPos ( std::size_t index ) const;
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Get the local bounding rectangle of the entity
+    ///
+    /// The returned rectangle is in local coordinates, which means
+    /// that it ignores the transformations (translation, rotation,
+    /// scale, ...) that are applied to the entity.
+    /// In other words, this function returns the bounds of the
+    /// entity in the entity's coordinate system.
+    ///
+    /// \return Local bounding rectangle of the entity
+    ///
+    ////////////////////////////////////////////////////////////
+    sf::FloatRect getLocalBounds ( void ) const;
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Get the global bounding rectangle of the entity
+    ///
+    /// The returned rectangle is in global coordinates, which means
+    /// that it takes in account the transformations (translation,
+    /// rotation, scale, ...) that are applied to the entity.
+    /// In other words, this function returns the bounds of the
+    /// sprite in the global 2D world's coordinate system.
+    ///
+    /// \return Global bounding rectangle of the entity
+    ///
+    ////////////////////////////////////////////////////////////
+    sf::FloatRect getGlobalBounds ( void ) const;
+
+    ////////////////////////////////////////////////////////////
+    // Accessor methods
+    ////////////////////////////////////////////////////////////
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Set the text's string
+    ///
+    /// A text's string is empty by default.
+    ///
+    /// \param sfString New string
+    ///
+    /// \see GetString
+    ///
+    ////////////////////////////////////////////////////////////
+    void SetString ( const sf::String& sfString );
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Set the text's font
+    ///
+    /// The \a font argument refers to a font that must
+    /// exist as long as the text uses it. Indeed, the text
+    /// doesn't store its own copy of the font, but rather keeps
+    /// a pointer to the one that you passed to this function.
+    /// If the font is destroyed and the text tries to
+    /// use it, the behaviour is undefined.
+    ///
+    /// \param oBmpFont New font
+    ///
+    /// \see GetFont
+    ///
+    ////////////////////////////////////////////////////////////
+    void SetFont ( const BmpFont& oBmpFont );
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Set the character scale
+    ///
+    /// The default scale is 1.f.
+    ///
+    /// \param fScale New character scale
+    ///
+    /// \see GetCharacterScale
+    ///
+    ////////////////////////////////////////////////////////////
+    void SetCharacterScale ( GLuint uiScale );
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Set the text's style
+    ///
+    /// You can pass a combination of one or more styles, for
+    /// example drimi::BmpText::Bold | drimi::BmpText::Italic.
+    /// The default style is drimi::BmpText::Regular.
+    ///
+    /// \param uiStyle New style
+    ///
+    /// \see GetStyle
+    ///
+    ////////////////////////////////////////////////////////////
+    void SetStyle ( sf::Uint32 uiStyle );
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Set the global color of the text
+    ///
+    /// By default, the text's color is opaque white.
+    ///
+    /// \param sfColor New color of the text
+    ///
+    /// \see GetColor
+    ///
+    ////////////////////////////////////////////////////////////
+    void SetColor ( const sf::Color& sfColor );
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Get the text's string
+    ///
+    /// \return Text's string
+    ///
+    /// \see SetString
+    ///
+    ////////////////////////////////////////////////////////////
+    const sf::String& GetString ( void ) const;
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Get the text's font
+    ///
+    /// If the text has no font attached, a NULL pointer is returned.
+    /// The returned reference is const, which means that you
+    /// cannot modify the font when you get it from this function.
+    ///
+    /// \return Pointer to the text's font
+    ///
+    /// \see SetFont
+    ///
+    ////////////////////////////////////////////////////////////
+    const BmpFont* GetFont ( void ) const;
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Get the character scale
+    ///
+    /// \return Scale of the characters
+    ///
+    /// \see SetCharacterScale
+    ///
+    ////////////////////////////////////////////////////////////
+    GLuint GetCharacterScale ( void ) const;
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Get the text's style
+    ///
+    /// \return Text's style
+    ///
+    /// \see SetStyle
+    ///
+    ////////////////////////////////////////////////////////////
+    sf::Uint32 GetStyle ( void ) const;
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Get the global color of the text
+    ///
+    /// \return Global color of the text
+    ///
+    /// \see SetColor
+    ///
+    ////////////////////////////////////////////////////////////
+    const sf::Color& GetColor ( void ) const;
+
+  private:
+
+    ////////////////////////////////////////////////////////////
+    // Internal methods
+    ////////////////////////////////////////////////////////////
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Draw the text to a render target
+    ///
+    /// \param target Render target to draw to
+    /// \param states Current render states
+    ///
+    ////////////////////////////////////////////////////////////
+    virtual void draw ( sf::RenderTarget& target, sf::RenderStates states ) const;
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Draw the underline
+    ///
+    ////////////////////////////////////////////////////////////
+    void DrawUnderline ( GLfloat fX, GLfloat fY, GLfloat fUnderlineOffset, GLfloat fUnderlineThickness );
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Update the text's geometry
+    ///
+    ////////////////////////////////////////////////////////////
+    void UpdateGeometry ( void );
+};
+
+} // namespace drimi
+
+#endif // DRIMI_BITMAPFONT_TEXT_HPP__
