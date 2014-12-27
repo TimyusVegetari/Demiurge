5eb0ab250d6ec0227f9812767557d999db9077a3
TimyusVegetari <ant110283@hotmail.fr>
2014-12-27 14:51:25 +0100

    Add a file for game development infomations
    Addition of a file to store basic informations relative to the development of the game as the name, the version, ...
    
    Signed-off-by: TimyusVegetari <ant110283@hotmail.fr>

---
+++ gamedev_info.hpp
@@ -0,0 +1,14 @@
+////////////////////////////////////////////////////////////
+//
+// This section is to modify informations relative to the game
+// development. By example, the name or the version of the game.
+//
+////////////////////////////////////////////////////////////
+
+#ifndef DEMIURGE_DEVINFO
+#define DEMIURGE_DEVINFO
+  #define DEMIURGE_NAME     "Demiurge"
+  #define DEMIURGE_VERSION  "0.2.1-alpha"
+  #define DEMIURGE_LICENSE  "GNU General Public License - version 2"
+  #define DEMIURGE_DATE     "2011-2014"
+#endif
