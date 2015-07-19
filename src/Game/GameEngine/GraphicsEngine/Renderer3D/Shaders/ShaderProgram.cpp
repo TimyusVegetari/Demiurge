cea32a6c42b6096dd8f87a86adbdaf8ab24c4f4d
TimyusVegetari <ant110283@hotmail.fr>
2015-07-19 19:29:02 +0200

    Add the ShaderProgram class.
    Addition of the shader program class.
    Addition of a header for the future shaders of the game.
    
    Signed-off-by: TimyusVegetari <ant110283@hotmail.fr>

---
+++ src/Game/GameEngine/GraphicsEngine/Renderer3D/Shaders/ShaderProgram.cpp
@@ -0,0 +1,117 @@
+////////////////////////////////////////////////////////////
+//
+// This file is part of Demiurge.
+// Copyright (C) 2011-2015 Acroute Anthony (ant110283@hotmail.fr)
+//
+// Demiurge is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// Demiurge is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Demiurge.  If not, see <http://www.gnu.org/licenses/>.
+//
+////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////
+// Headers
+////////////////////////////////////////////////////////////
+#include <Game/GameEngine/GraphicsEngine/Renderer3D/Shaders/ShaderProgram.hpp>
+
+////////////////////////////////////////////////////////////
+// Constructor(s)/Destructor
+////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////
+ShaderProgram::ShaderProgram ( void ) :
+  m_gVertexShader   (),
+  m_gFragmentShader (),
+  m_uiID            (0)
+{
+}
+
+////////////////////////////////////////////////////////////
+ShaderProgram::~ShaderProgram ( void ) {
+  Delete ();
+}
+
+////////////////////////////////////////////////////////////
+// General methods
+////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////
+GLboolean ShaderProgram::Load ( const std::string szVertexName, const std::string szFragmentName ) {
+  // Loading of vertex and fragment shaders
+  if (m_gVertexShader.Load (GL_VERTEX_SHADER, szVertexName) && m_gFragmentShader.Load (GL_FRAGMENT_SHADER, szFragmentName)) {
+    m_uiID = glCreateProgram ();
+
+    glAttachShader (m_uiID, m_gVertexShader.GetID ());
+    glAttachShader (m_uiID, m_gFragmentShader.GetID ());
+
+    glLinkProgram (m_uiID);
+
+    return CheckCompilation ();
+  }
+
+  return GL_FALSE;
+}
+
+////////////////////////////////////////////////////////////
+void ShaderProgram::Delete ( void ) {
+  if (m_uiID != 0) {
+    glDetachShader (m_uiID, m_gFragmentShader.GetID ());
+    glDetachShader (m_uiID, m_gVertexShader.GetID ());
+    glDeleteProgram (m_uiID);
+    m_uiID = 0;
+  }
+
+  // Deleting of the shaders
+  m_gFragmentShader.Delete ();
+  m_gVertexShader.Delete ();
+}
+
+////////////////////////////////////////////////////////////
+// Accessor methods
+////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////
+GLuint ShaderProgram::GetID ( void ) {
+  return m_uiID;
+}
+
+////////////////////////////////////////////////////////////
+// Internal methods
+////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////
+GLboolean ShaderProgram::CheckCompilation ( void ) {
+  GLint iCompilationStatus = 0;
+
+  glGetProgramiv (m_uiID, GL_LINK_STATUS, &iCompilationStatus);
+  if (iCompilationStatus != GL_TRUE) {
+    std::cout << "Error : Compilation of a shader program !" << std::endl;
+    // What is the cause of the failure ?
+    GLint iLogLength = 0;
+    GLchar* cLog = NULL;
+
+    glGetProgramiv (m_uiID, GL_INFO_LOG_LENGTH, &iLogLength);
+    cLog = new GLchar[iLogLength];
+    if (cLog == NULL) {
+      std::cout << "Allocation memory error for the log of the shader program compilation !" << std::endl;
+      return GL_FALSE;
+    }
+    // Getting and display of the log
+    glGetProgramInfoLog (m_uiID, iLogLength, &iLogLength, cLog);
+    std::cout << cLog << std::endl;
+    delete[] cLog;
+
+    return GL_FALSE;
+  }
+
+  return GL_TRUE;
+}
